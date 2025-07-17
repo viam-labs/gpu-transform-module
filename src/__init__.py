feab7a92b47cec5a23b1838affea305099043a46
Isha Yerramilli-Rao <isha.yerramilli-rao@viam.com>
2025-07-17 15:30:42 -0400

    removed unused files / cleanup

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from viam.resource.registry import Registry, ResourceCreatorRegistration
-from .camera_module import GPUTransformCamera
-from .transform_pipeline import GPUTransformPipeline
-from viam.components.camera import Camera
-
-Registry.register_resource_creator(Camera.API, GPUTransformCamera.MODEL, ResourceCreatorRegistration(GPUTransformCamera.new))
-
-__all__ = ['GPUTransformCamera', 'GPUTransformPipeline']
